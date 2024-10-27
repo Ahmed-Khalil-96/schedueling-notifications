dfcb11500e84594819e0853f53878f35f90bc596
ahmed-khalil-96 <ahmed.elabassiry96@gmail.com>
2024-10-27 10:38:19 +0300

    fixing bugs

--- src/initApp.js
+++ src/initApp.js
@@ -7,7 +7,7 @@ import job from './modules/notifications/notification.controller.js'
 
 export const initApp = (app , express)=>{
 connection()
-// job
+job
 app.use(express.json())
 app.get("/",(req,res)=>{
     res.status(200).json("Server is running")
